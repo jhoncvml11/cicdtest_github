668f8e999456a6fc9eaf9374b45bd473a005ac09
jhon <jhon.cardenas@vml.com>
2025-04-09 22:49:32 -0500

    unit test ok

---
+++ src/app/tests/counter.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Home from "../page";
+
+
+describe("Contador", () => {
+  it("inicia en 0", () => {
+    render(<Home />);
+    expect(screen.getByTestId("counter-value")).toHaveTextContent("Valor: 0");
+  });
+
+  it("incrementa en 1 al hacer clic en +", async () => {
+    render(<Home />);
+    await userEvent.click(screen.getByTestId("increment"));
+    expect(screen.getByTestId("counter-value")).toHaveTextContent("Valor: 1");
+  });
+
+  it("decrementa en 1 al hacer clic en -", async () => {
+    render(<Home />);
+    await userEvent.click(screen.getByTestId("decrement"));
+    expect(screen.getByTestId("counter-value")).toHaveTextContent("Valor: -1");
+  });
+});
