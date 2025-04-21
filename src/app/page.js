5f37ae3a670b27f08fd0e8df898f8a891595eeb5
jhon <jhon.cardenas@vml.com>
2025-04-21 17:05:54 -0500

    new styles btns

--- src/app/page.js
+++ src/app/page.js
@@ -8,10 +8,10 @@ export default function Home() {
     <main style={{ padding: "2rem", textAlign: "center" }}>
       <h1>Contador</h1>
       <p data-testid="counter-value">Valor: {count}</p>
-      <button onClick={() => setCount(count + 1)} data-testid="increment">
+      <button className="px-8 py-2 bg-blue-500 cursor-pointer mr-2" onClick={() => setCount(count + 1)} data-testid="increment">
         +
-      </button>
-      <button onClick={() => setCount(count - 1)} data-testid="decrement">
+      </button >
+      <button className="px-8 py-2 bg-blue-500 cursor-pointer ml-2" onClick={() => setCount(count - 1)} data-testid="decrement">
         -
       </button>
     </main>
