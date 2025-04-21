e4ad018b3bf426ff49a9087f8ed73b758b340ea9
jhon <jhon.cardenas@vml.com>
2025-04-21 16:39:29 -0500

    fix route un unit test

--- tests/unit/counter.test.js
+++ tests/unit/counter.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Home from "../page";
+import Home from "../../src/app/page";
 
 
 describe("Contador", () => {
